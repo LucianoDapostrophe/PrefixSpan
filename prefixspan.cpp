31b3940e98561ddc78792bcae9a783600e22779d
LucianoDapostrophe <90058982+LucianoDapostrophe@users.noreply.github.com>
2024-11-16 19:53:40 -0700

    just a tiny bit tidier

--- prefixspan.cpp
+++ prefixspan.cpp
@@ -2,26 +2,20 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <map>
+//#include <map>
 
 struct seqPattern {
     std::string prefix;
     std::vector<seqPattern>* suffixes;
 };
 
-void prefixSpan(std::map<int, std::vector<char>>* s, long double minSup) {
+void prefixSpan(std::vector<std::string> sequences, std::vector<std::string> buildActions, long double minSup) {
 
 }
 
-int main() {
-    //generate tree
-    std::vector<std::string> sequences;
-    std::string tmp = "";
-    while (std::getline(std::cin >> std::ws, tmp)) {
-        sequences.push_back(tmp);
-    }
-    std::vector<std::string> buildActions;
+void getActionsFromDict(std::vector<std::string>& buildActions) {
     std::ifstream myFile("dictionary.txt");
+    std::string tmp;
     if (myFile.is_open()) {
         while (myFile.good()) {
             std::getline(myFile, tmp);
@@ -35,6 +29,17 @@ int main() {
         }
         myFile.close();
     }
+}
+
+int main() {
+    //generate tree
+    std::vector<std::string> sequences;
+    std::string tmp = "";
+    while (std::getline(std::cin >> std::ws, tmp)) {
+        sequences.push_back(tmp);
+    }
+    std::vector<std::string> buildActions;
+    getActionsFromDict(buildActions);
     for (std::string n : buildActions) {
         std::cout << n << std::endl;
     }
