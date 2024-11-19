e448a99cfce0d8cceb1b579536a1163f91fee459
LucianoDapostrophe <90058982+LucianoDapostrophe@users.noreply.github.com>
2024-11-19 14:30:53 -0700

    handling itemsets (events that happen 'simultaneously') within time series

--- prefixspan.cpp
+++ prefixspan.cpp
@@ -6,12 +6,21 @@
 #include <set>
 #include <limits>
 
+using Sequence = std::vector<std::vector<int>>;
+using Dataset = std::vector<Sequence>;
+
 // Helper function to display sequences
-void printSequences(const std::vector<std::vector<int>>& sequences) {
+void printSequences(const Dataset& sequences) {
     for (const auto& seq : sequences) {
         std::cout << "[";
-        for (size_t i = 0; i < seq.size(); ++i) {
-            std::cout << seq[i];
+        for (int i = 0; i < seq.size(); i++) {
+            std::cout << "{";
+            for (int j = 0; j < seq[i].size(); j++)
+            {
+                std::cout << seq[i][j];
+                if (i < seq[i].size() - 1) std::cout << ", ";
+            }
+            std::cout << "}";
             if (i < seq.size() - 1) std::cout << ", ";
         }
         std::cout << "]" << std::endl;
@@ -19,28 +28,32 @@ void printSequences(const std::vector<std::vector<int>>& sequences) {
 }
 
 // project the dataset based on a prefix
-std::vector<std::vector<int>> projectDatabase(const std::vector<std::vector<int>>& dataset, int prefix) {
-    std::vector<std::vector<int>> projectedDatabase;
-    for (const std::vector<int>& sequence : dataset) {
-        auto it = find(sequence.begin(), sequence.end(), prefix);
-        if (it != sequence.end() && ++it != sequence.end()) {
-            std::vector<int> projectedSequence(it, sequence.end());
-            projectedDatabase.push_back(projectedSequence);
+Dataset projectDatabase(const Dataset& dataset, const std::vector<int>& prefix) {
+    Dataset projectedDatabase;
+    for (const auto& sequence : dataset) {
+        for (int i = 0; i < sequence.size(); i++) {
+            if (sequence[i] == prefix) {
+                if (i + 1 < sequence.size()) {
+                    Sequence projectedSequence(sequence.begin() + i + 1, sequence.end());
+                    projectedDatabase.push_back(projectedSequence);
+                }
+                break;
+            }
         }
     }
     return projectedDatabase;
 }
 
 // find patterns occuring >= the support
-std::map<int, int> findFrequent(const std::vector<std::vector<int>>& dataset, int minSupport) {
-    std::map<int, int> frequency;
-    for (const std::vector<int>& sequence : dataset) {
-        std::set<int> uniqueItems(sequence.begin(), sequence.end());
-        for (int item : uniqueItems) {
-            frequency[item]++;
+std::map<std::vector<int>, int> findFrequent(const Dataset& dataset, int minSupport) {
+    std::map<std::vector<int>, int> frequency;
+    for (const auto& sequence : dataset) {
+        std::set<std::vector<int>> uniqueItems(sequence.begin(), sequence.end());
+        for (const auto& itemset : uniqueItems) {
+            frequency[itemset]++;
         }
     }
-    std::map<int, int> frequentItems;
+    std::map<std::vector<int>, int> frequentItems;
     for (const auto& item : frequency) {
         if (item.second >= minSupport) {
             frequentItems[item.first] = item.second;
@@ -49,12 +62,12 @@ std::map<int, int> findFrequent(const std::vector<std::vector<int>>& dataset, in
     return frequentItems;
 }
 
-void prefixSpan(const std::vector<std::vector<int>>& dataset, const std::vector<int>& prefix, int minSup) {
+void prefixSpan(const Dataset& dataset, const Sequence& prefix, int minSup) {
     //find frequent items in dataset
-    std::map<int, int> frequentItems = findFrequent(dataset, minSup);
+    std::map<std::vector<int>, int> frequentItems = findFrequent(dataset, minSup);
     //iterate over each frequent item
     for (const auto& item : frequentItems) {
-        std::vector<int> newPrefix = prefix;
+        Sequence newPrefix = prefix;
         newPrefix.push_back(item.first);
 
         //output for debugging
@@ -62,39 +75,13 @@ void prefixSpan(const std::vector<std::vector<int>>& dataset, const std::vector<
         printSequences({newPrefix});
 
         //project database from the current prefix
-        std::vector<std::vector<int>> projectedDatabase = projectDatabase(dataset, item.first);
+        Dataset projectedDatabase = projectDatabase(dataset, item.first);
 
         //recursive call
         if (!projectedDatabase.empty()) {
             prefixSpan(projectedDatabase, newPrefix, minSup);
         }
     }
-}
-
-std::vector<std::vector<int>> convertToInts(const std::vector<std::string>& data) {
-    std::string tmp = "";
-    std::vector<std::vector<int>> dataset;
-    for (int i = 0; i < data.size(); i++) {
-        dataset.push_back(std::vector<int>());
-        int j = 0;
-        while (j < data[i].length())
-        {
-            if (data[i][j] == '-') {
-                j += 3;
-                continue;
-            }
-            if (data[i][j] == ' ' && tmp != "") {
-                dataset[i].push_back(std::stoi(tmp));
-                tmp = "";
-            }
-            else {
-                tmp += data[i][j];
-            }
-            j++;
-        }
-        
-    }
-    return dataset;
 }
 
 int handleArgs(int argc, char** argv) {
@@ -110,23 +97,34 @@ int main(int argc, char** argv) {
     std::cout << "Have " << argc << " arguments:\n";
     int minSupport = handleArgs(argc, argv);
     //generate tree
-    std::vector<std::string> data;
-    std::string tmp = "";
-    while (std::getline(std::cin >> std::ws, tmp)) {
-        data.push_back(tmp);
-    }
-    for (std::string s : data) std::cout << s << std::endl << std::endl;
-    std::vector<std::vector<int>> dataset = convertToInts(data);
-    //debug
-    for (int i = 0; i < dataset.size(); i++)
-    {
-        for (int j = 0; j < dataset[i].size(); j++)
-        {
-            std::cout << dataset[i][j] << " ";
+    Dataset dataset;
+    dataset.push_back(Sequence());
+    dataset[0].push_back(std::vector<int>());
+    int i = 0, j = 0;
+    int num;
+    while (std::cin >> num) {
+        if (num == -2) {
+            dataset.push_back(Sequence());
+            dataset[i].pop_back();
+            std::cout << "new sequence" << std::endl;
+            i++;
+            j = 0;
+            dataset[i].push_back(std::vector<int>());
         }
-        std::cout << std::endl;
+        else if (num == -1) {
+            dataset[i].push_back(std::vector<int>());
+            std::cout << "new itemset" << std::endl;
+            j++;
+        }
+        else {
+            dataset[i][j].push_back(num);
+            std::cout << "number entered" << std::endl;
+        }
+        printSequences(dataset);
     }
-    std::vector<int> prefix;
+    dataset.pop_back();
+    //debug
+    Sequence prefix;
     std::cout << "running prefixspan with minsupport = " << minSupport << std::endl;
     //output tree
     prefixSpan(dataset, prefix, minSupport);
